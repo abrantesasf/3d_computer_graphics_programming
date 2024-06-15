a3bb462f9611069a029fccd2f14c481cdf89ff49
Abrantes Araújo Silva Fillho <abrantesasf@pm.me>
2024-06-15 13:06:23 -0300

    Fiat Lux

---
+++ 3drenderer/src/main.c
@@ -0,0 +1,123 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <SDL2/SDL.h>
+
+// Declarações globais:
+SDL_Window *janela = NULL;             // ponteiro para uma janela
+SDL_Renderer *renderizador = NULL;     // ponteiro para um renderizador
+bool esta_rodando = false;             // status de execução do programa
+
+// Protótipos de funções:
+bool inicializar_janela(void);         // inicializa uma janela
+void configurar(void);                 // setup inicial da aplicação
+void processar_input(void);            // recebe e processa inputs do usuário
+void atualizar(void);                  // atualiza o estado do programa
+void renderizar(void);                 // renderiza a aplicação
+
+
+// Função main:
+int main(void)
+{
+    esta_rodando = inicializar_janela();
+
+    configurar();
+
+    while (esta_rodando)
+    {
+        processar_input();
+        atualizar();
+        renderizar();
+    }
+
+    return 0;
+}
+
+
+
+// Definição das funções:
+bool inicializar_janela(void)
+{
+    // Inicializa o ambiente SDL. Podemos inicializar os
+    // gráficos, o mouse, o teclado, etc. Iremos, no momento,
+    // inicializar tudo:
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    {
+        fprintf(stderr, "Erro na inicialização do SDL.\n");
+        return false;
+    }
+
+    // Agora temos que criar uma janela SDL. A função que cria a janela
+    // tem 6 parâmetros: título, x, y, w, h, flags. Se o título for null,
+    // a janela não terá um título.
+    janela = SDL_CreateWindow(NULL,
+                              SDL_WINDOWPOS_CENTERED,
+                              SDL_WINDOWPOS_CENTERED,
+                              800,
+                              600,
+                              SDL_WINDOW_BORDERLESS);
+    if (!janela)
+    {
+        fprintf(stderr, "Erro na criação da janela SDL.\n");
+        return false;
+    }
+
+    // Agora temos que criar um renderer para a janela que foi criada,
+    // um renderer que vai acompanhar nossa janela. Temos que passar
+    // o ponteiro para a janela, o display onde a janela será exibida (-1
+    // significa o display padrão), e flags (0 significa que não tem
+    // nenhuma flag especial).
+    renderizador = SDL_CreateRenderer(janela, -1, 0);
+    if (!renderizador)
+    {
+        fprintf(stderr, "Erro na criação do renderizador SDL.\n");
+    }
+
+    // Se chegamos aqui, conseguimos inicializar o SDL, criamos uma
+    // janela e acoplamos um renderizador para essa janela.
+    return true;
+}
+
+
+void configurar(void)
+{
+    
+}
+
+void processar_input(void)
+{
+    // Structure para receber os eventos
+    SDL_Event evento;
+
+    // Recebe o evento:
+    SDL_PollEvent(&evento);
+
+    // Testa o evento recebido:
+    switch (evento.type)
+    {
+        case SDL_QUIT:
+            esta_rodando = false;
+            break;
+        case SDL_KEYDOWN:
+            if (evento.key.keysym.sym == SDLK_ESCAPE)
+                esta_rodando = false;
+            break;
+    }
+}
+
+void atualizar(void)
+{
+    
+}
+
+void renderizar(void)
+{
+    // Ajusta a cor do renderer. Tem 5 parâmetros: o renderer, os valores
+    // RGB e o valor do alpha (transparência)
+    SDL_SetRenderDrawColor(renderizador, 255, 0, 0, 255);
+
+    // Agora vamos limpar tudo:
+    SDL_RenderClear(renderizador);
+
+    // E agora vamos renderizar:
+    SDL_RenderPresent(renderizador);
+}
--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# 3d_computer_graphics_programming
-Repositório para o curso 3D Computer Graphics Programming, do Pikuma (https://pikuma.com)
+# 3D Graphics Programming
+
+Repositório para minhas atividades no curso "3D Graphics Programming",
+do Gustavo Pezzi, no Pikuma ([https://pikuma.com](https://pikuma.com)).
+
+O objetivo do curso é aprender a teoria e a matemática que suportam a
+criação de gráficos tridimensionais, através da criação prática de um
+software renderizador a partir do zero, usando apenas a linguagem
+[C padrão](https://en.wikipedia.org/wiki/C_(programming_language)) e
+a biblioteca [SDL (Simple DirectMedia)](https://www.libsdl.org/). Maiores
+informações na página do curso:
+[3D Graphics Programming](https://pikuma.com/courses/learn-3d-computer-graphics-programming).
+
+Progresso do curso:
+
+- [] Introduction
+- [] Compiling our project
+- [] SDL Setup and Color Buffer
+- [] Multiple Header Files
+- [] Vectors and Points
+- [] Projecting Points
+- [] Linear Transformations
+- [] Fixed Time Step
+- [] Triangles and Vertices
+- [] Line Drawing
+- [] Dynamic Arrays
+- [] OBJ Files
+- [] Vector Operations
+- [] Triangle Rasterization
+- [] Sorting Faces by Depth
+- [] Matrices
+- [] 3D Matrix Transformations
+- [] Projection Matrix
+- [] Light and Shading
+- [] Texture Mapping
+- [] Perspective Correct Textures
+- [] Decoing PNG Files
+- [] Textures OBJ Files
+- [] Z-Buffer
+- [] Camera
+- [] Clipping
+- [] Code Polishing & Refactoring
+- [] Multiple Meshes
+- [] CPU vs. GPU Rasterization
+- [] Conclusion and Next Steps
